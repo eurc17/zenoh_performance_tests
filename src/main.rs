9552a1a91a9ffef85cbb321abdc3954d27873956
Yi-Hung Kuo <50904241+eurc17@users.noreply.github.com>
2021-12-20 16:27:10 +0800

    Update base code with multi pub to 1 sub test

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,35 @@ use std::{sync::*, time::*};
 #[async_std::main]
 async fn main() {
     let zenoh = Arc::new(Zenoh::new(net::config::default()).await.unwrap());
-    let start_until = Instant::now() + Duration::from_millis(100);
-    let until = start_until + Duration::from_millis(100);
-    let _timeout = until.saturating_duration_since(Instant::now());
-    async_std::task::spawn(subscribe_worker(zenoh.clone(), start_until));
+    let start = Instant::now();
+    let start_until = start + Duration::from_millis(100);
+    let timeout = start_until + Duration::from_millis(100);
+    let sub_handle = async_std::task::spawn(subscribe_worker(zenoh.clone(), start_until, timeout));
     async_std::task::sleep(std::time::Duration::from_millis(50)).await;
     // async_std::task::spawn(publish_worker(zenoh.clone(), start_until));
+    let total_number = 100000;
     let pub_futures =
-        (0..10000).map(|peer_index| publish_worker(zenoh.clone(), start_until, peer_index));
+        (0..total_number).map(|peer_index| publish_worker(zenoh.clone(), start_until, peer_index));
     futures::future::try_join_all(pub_futures).await.unwrap();
-    async_std::task::sleep(std::time::Duration::from_secs(1)).await;
+
+    // async_std::task::sleep(std::time::Duration::from_secs(1)).await;
+    let result = async_std::future::timeout(Duration::from_millis(1000), sub_handle).await;
+    if result.is_err() {
+        println!("All messages delivered!");
+    } else {
+        // for change in result.unwrap().iter() {
+        //     println!(
+        //         ">> {:?} for {} : {:?} at {}",
+        //         change.kind, change.path, change.value, change.timestamp
+        //     );
+        // }
+        let change_vec = result.unwrap();
+        println!(
+            "total received messages: {}/{}",
+            change_vec.len(),
+            total_number
+        );
+    }
 }
 
 async fn publish_worker(zenoh: Arc<Zenoh>, start_until: Instant, peer_id: usize) -> Result<()> {
@@ -34,17 +53,29 @@ async fn publish_worker(zenoh: Arc<Zenoh>, start_until: Instant, peer_id: usize)
     Ok(())
 }
 
-async fn subscribe_worker(zenoh: Arc<Zenoh>, start_until: Instant) {
+async fn subscribe_worker(
+    zenoh: Arc<Zenoh>,
+    start_until: Instant,
+    timeout: Instant,
+) -> Vec<Change> {
     async_std::task::sleep(start_until - Instant::now()).await;
+    let mut change_vec = vec![];
     let workspace = zenoh.workspace(None).await.unwrap();
     let mut change_stream = workspace
         .subscribe(&"/demo/example/**".try_into().unwrap())
         .await
         .unwrap();
     while let Some(change) = change_stream.next().await {
-        println!(
-            ">> {:?} for {} : {:?} at {}",
-            change.kind, change.path, change.value, change.timestamp
-        )
+        // println!(
+        //     ">> {:?} for {} : {:?} at {}",
+        //     change.kind, change.path, change.value, change.timestamp
+        // );
+        if Instant::now() < timeout {
+            change_vec.push(change);
+        } else {
+            println!("Timeout reached");
+            break;
+        }
     }
+    change_vec
 }
