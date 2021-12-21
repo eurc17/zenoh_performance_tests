9bfa3959eb7eda7ecbc815745209a54c76a1df4d
Yi-Hung Kuo <50904241+eurc17@users.noreply.github.com>
2021-12-21 17:19:34 +0800

    Added support for multiple subscriber worker

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use zenoh::*;
 
 use anyhow::Result;
 use futures::{self, prelude::*};
+use rayon::prelude::*;
 use std::{sync::*, time::*};
 
 #[async_std::main]
@@ -11,16 +12,36 @@ async fn main() {
     let start = Instant::now();
     let start_until = start + Duration::from_millis(100);
     let timeout = start_until + Duration::from_millis(100);
-    let sub_handle = async_std::task::spawn(subscribe_worker(zenoh.clone(), start_until, timeout));
+    let total_sub_number = 10;
+
+    let sub_handle_vec = (0..total_sub_number)
+        .into_par_iter()
+        .map(|peer_id: usize| {
+            let sub_handle = async_std::task::spawn(subscribe_worker(
+                zenoh.clone(),
+                start_until,
+                timeout,
+                peer_id,
+            ));
+            sub_handle
+        })
+        .collect::<Vec<_>>();
     async_std::task::sleep(std::time::Duration::from_millis(50)).await;
     // async_std::task::spawn(publish_worker(zenoh.clone(), start_until));
-    let total_number = 100000;
-    let pub_futures =
-        (0..total_number).map(|peer_index| publish_worker(zenoh.clone(), start_until, peer_index));
+    let total_put_number = 100000;
+    let pub_futures = (0..total_put_number)
+        .map(|peer_index| publish_worker(zenoh.clone(), start_until, peer_index));
     futures::future::try_join_all(pub_futures).await.unwrap();
 
     // async_std::task::sleep(std::time::Duration::from_secs(1)).await;
-    let result = async_std::future::timeout(Duration::from_millis(1000), sub_handle).await;
+    let sub_handle_fut = sub_handle_vec
+        .into_iter()
+        .map(|sub_handle| async_std::future::timeout(Duration::from_millis(1000), sub_handle))
+        .collect::<Vec<_>>();
+
+    let result = futures::future::try_join_all(sub_handle_fut).await;
+
+    // let result = async_std::future::timeout(Duration::from_millis(1000), sub_handle).await;
     if result.is_err() {
         println!("All messages delivered!");
     } else {
@@ -30,12 +51,15 @@ async fn main() {
         //         change.kind, change.path, change.value, change.timestamp
         //     );
         // }
-        let change_vec = result.unwrap();
-        println!(
-            "total received messages: {}/{}",
-            change_vec.len(),
-            total_number
-        );
+        let result_vec = result.unwrap();
+        for (id, change_vec) in result_vec.iter() {
+            println!(
+                "sub peer {}: total received messages: {}/{}",
+                id,
+                change_vec.len(),
+                total_put_number
+            );
+        }
     }
     let msg_payload = format!("Hello World from peer {:08}", 1 as usize);
     let payload_size = std::mem::size_of_val(&msg_payload);
@@ -60,7 +84,8 @@ async fn subscribe_worker(
     zenoh: Arc<Zenoh>,
     start_until: Instant,
     timeout: Instant,
-) -> Vec<Change> {
+    peer_id: usize,
+) -> (usize, Vec<Change>) {
     async_std::task::sleep(start_until - Instant::now()).await;
     let mut change_vec = vec![];
     let workspace = zenoh.workspace(None).await.unwrap();
@@ -80,5 +105,5 @@ async fn subscribe_worker(
             break;
         }
     }
-    change_vec
+    (peer_id, change_vec)
 }
