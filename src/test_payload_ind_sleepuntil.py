e2c0f370a725854c1b82bdf389cec2e2f1364727
eurc17 <50904241+eurc17@users.noreply.github.com>
2022-05-04 13:09:39 +0800

    Fixed program timeout

--- src/test_payload_ind_sleepuntil.py
+++ src/test_payload_ind_sleepuntil.py
@@ -31,11 +31,7 @@ def main(args):
     round_timeout = args.round_timeout  # ms
     startup_delay = max((10 * args.peer_num) / 1000, 2)
     program_timeout = (
-        get_sleep()
-        + round_timeout / 1000
-        + 10
-        + (args.init_time) / 1000
-        + startup_delay
+        round_timeout / 1000 + 10 + (args.init_time) / 1000 + startup_delay
     )  # s
     sleep_until = subprocess.run(
         ["which", "sleepuntil"], stdout=subprocess.PIPE
@@ -54,7 +50,7 @@ def main(args):
         args.payload_size_start, args.payload_size_end + 1, args.payload_size_step
     ):
         if sleep_until == "" or True:
-            actual_program_timeout = program_timeout
+            actual_program_timeout = program_timeout + get_sleep()
         else:
             actual_program_timeout = program_timeout + get_sleep()
         # file_name = "{}-{}-{}-{}-{}-{}".format(
