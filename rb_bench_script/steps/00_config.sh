1ea9f1fd61376f5cd44533e4e455b78544282d07
jerry73204 <jerry73204@gmail.com>
2022-05-10 10:46:42 +0800

    Tweak RB params

--- rb_bench_script/steps/00_config.sh
+++ rb_bench_script/steps/00_config.sh
@@ -11,14 +11,15 @@ log_time="$(date +'%Y-%m-%d-%T')"
 remote_dir="/home/pi/rb-exp"
 
 ## The directory to store program outputs on RPi
-remote_output_dir="$remote_dir/test/payload/log_exp_$log_time"
+output_dir_suffix="test/payload/log_exp_$log_time"
+remote_output_dir="$remote_dir/$output_dir_suffix"
 
 ## Zenoh repo and commit for compiling zenohd
 zenoh_git_url="https://github.com/eclipse-zenoh/zenoh.git"
 zenoh_git_commit="90539129b1a7c9e8c7d7daaa84138d093f71fedf"
 
 ## Space-delimited payload sizes
-payload_sizes=$(cat "$repo_dir/scripts/exp_payload_list.txt")
+payload_sizes=$(cat "$script_dir/../scripts/exp_payload_list.txt")
 # payload_sizes="4096"
 
 ## The RUST_LOG env set on RPi. It is intended for debug purpose.
--- rb_bench_script/steps/07_download_logs.sh
+++ rb_bench_script/steps/07_download_logs.sh
@@ -4,14 +4,14 @@ set -e
 while read addr port peer_id name
 do
     {
-        remote_log_dir="$remote_dir/test/"
-        local_log_dir="$script_dir/exp_logs/$name/log_exp_${log_time}/test"
+        echo $addr $port
+        local_dir="$script_dir/exp_logs/$name/log_exp_${log_time}/$output_dir_suffix"
         
-        mkdir -p "$local_log_dir"
+        mkdir -p "$local_dir"
         rsync -aPz -e "ssh -p $port" \
-              "pi@$addr:$remote_log_dir" \
-              "$local_log_dir"
-    } >/dev/null 2>&1 &
+              "pi@$addr:$remote_output_dir" \
+              "$local_dir"
+    } &
 done < "$script_dir/config/rpi_addrs.txt"
 
 wait
