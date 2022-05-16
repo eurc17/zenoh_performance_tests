607de7179a89adadb88be128464876a0499ba0f6
jerry73204 <jerry73204@gmail.com>
2022-05-17 06:31:46 +0800

    Save RB params

--- rb_bench_script/run_many.sh
+++ rb_bench_script/run_many.sh
@@ -8,23 +8,38 @@ pushd "$script_dir"
 
 while read -u10 rsize esize psize
 do
-    export rsize
-    export esize
-    export psize
-    
-    ts="$(date --rfc-3339=seconds | tr ' ' 'T')"
-    name="${ts}_round-${rsize}_echo-${esize}_payload-${psize}"
+# for rsize in 20 50
+# do
+#     for esize in 10
+#     do
+#         for psize in 128 256 512 1024 2048 4096 8192
+#         do
+            if [ "$rsize" -le "$esize" ]
+            then
+                continue
+            fi
+            
+            export rsize
+            export esize
+            export psize
+            
+            ts="$(date --rfc-3339=seconds | tr ' ' 'T')"
+            name="${ts}_pi5x5_round-${rsize}_echo-${esize}_payload-${psize}"
 
-    echo "Running experiment $name"
+            echo "Running experiment $name"
 
-    ./kill_remote.sh
-    ./run.sh
+            ./kill_remote.sh
+            ./run.sh
 
-    mkdir -p "$name"
-    mv exp_logs "$name"
-    ./kill_remote.sh
+            mkdir -p "$name"
+            mv exp_logs "$name"
+            ./kill_remote.sh
 
-    sleep 10
+#             sleep 10
+#         done
+#     done
+# done
+
 done 10<"$script_dir/config/params.txt"
 
 popd
