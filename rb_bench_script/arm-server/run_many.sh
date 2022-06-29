7dbffafb632d1ac43d22369f4fc26ef8cd93eb67
aeon <jerry73204@gmail.com>
2022-06-29 19:16:11 +0800

    Change RB test script

--- rb_bench_script/arm-server/run_many.sh
+++ rb_bench_script/arm-server/run_many.sh
@@ -6,11 +6,13 @@ trap 'kill $(jobs -p)' INT
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 pushd "$script_dir"
 
+exp_dt="$(date --rfc-3339=ns | tr ' ' 'T')"
+
 # while read -u10 rsize esize psize
 # do
-for rsize in 3
+for rsize in 10
 do
-    for esize in 0 1
+    for esize in 0 2 4
     do
         for psize in 128 256 512 1024 2048 4096 8192
         do
@@ -18,13 +20,15 @@ do
             then
                 continue
             fi
-            
+
+            exp_name="${exp_dt}_unlimited-zenoh_round-${rsize}_echo-${esize}_payload-${psize}"
+
             export rsize
             export esize
             export psize
-            
+            export exp_name
+
             ts="$(date --rfc-3339=seconds | tr ' ' 'T')"
-            name="${ts}_pi5x5_round-${rsize}_echo-${esize}_payload-${psize}"
 
             echo "Running experiment $name"
 
